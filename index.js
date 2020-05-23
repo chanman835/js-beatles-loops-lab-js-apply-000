d69654f3c1cbf6c29683f696d6d50e960348b8a2
Winson Chan <chanw835@gmail.com>
2020-05-23 21:00:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function theBeatlesPlay(musicians,instruments) {
-  
+  var product = [];
+  for (let i = 0; i < musicians.length; i++) {
+    let singleMusician = musicians[i];
+    let singleInstrument = instruments[i];
+    product.push()
+  }
   
 }
